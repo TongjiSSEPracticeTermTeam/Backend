7bee3edcfaa372a20a89fb0059a59cce409a526f
Zhang Yao <cinea@cinea.com.cn>
2023-07-07 16:51:39 +0800

    Update Program.cs

--- Cinema/Program.cs
+++ Cinema/Program.cs
@@ -85,10 +85,10 @@ builder.Services.AddHttpContextAccessor();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI();
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
     app.UseCors("CorsPolicy");
 }
 else
