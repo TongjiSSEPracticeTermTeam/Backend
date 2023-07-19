b0f0324c0c6d9094ba6344e87c8455068c103f53
Raymond <raymond4ever@163.com>
2023-07-19 19:52:42 +0800

    完成电影Movie基本增删查API

--- Cinema/Controllers/MovieController.cs
+++ Cinema/Controllers/MovieController.cs
@@ -1,6 +1,6 @@
-﻿using Cinema.Entities;
+﻿using Cinema.DTO.MovieService;
+using Cinema.Entities;
 using Microsoft.AspNetCore.Mvc;
-using Cinema.DTO.MovieService;
 using Microsoft.EntityFrameworkCore;
 
 namespace Cinema.Controllers;
@@ -15,35 +15,184 @@ public class MovieController
     private readonly CinemaDb _db;
 
     /// <summary>
-    /// 初始化
+    /// 控制器构造函数
     /// </summary>
-    /// <param name="="db"><param>
+    /// <param name="db"></param>
     public MovieController(CinemaDb db)
     {
         _db = db;
     }
 
     /// <summary>
-    ///根据电影id获取电影演员信息
+    /// 获得所有电影院信息
     /// </summary>
     /// <returns>
-    /// 返回电影演员json列表
+    /// 返回电影json列表
     /// </returns>
     [HttpGet]
-    public async Task<IActionResult> GetActorList(String id)
+    public async Task<IActionResult> GetMovie()
     {
-        var tmp_movie = await _db.Movies.Where(t => 
-        (t.MovieId == id)).FirstOrDefaultAsync()!;
-        var tmp_actors = tmp_movie!.Acts;
+        return new JsonResult(await _db.Movies.ToListAsync());
+    }
 
-        return new JsonResult(new GetActorsByMovieIdResponse
+    /// <summary>
+    /// 根据id获得指定电影信息
+    /// </summary>
+    /// <param name="id">电影id</param>
+    /// <returns>
+    /// 返回电影json
+    /// </returns>
+    [HttpGet("getMovieById/{id}")]
+    public async Task<IActionResult> GetMovieById(string id)
+    {
+        var movie = await _db.Movies.FindAsync(id);
+        if (movie == null)
+        {
+            return new JsonResult(new GetMovieByIdResponse
+            {
+                Status = "4001",
+                Message = "该电影不存在"
+            });
+        }
+
+        return new JsonResult(new GetMovieByIdResponse
         {
             Status = "10000",
             Message = "查询成功",
-            Act = tmp_actors,
+            Movie = movie
         });
-
-        ///return new JsonResult(tmp_actors.ToList());
     }
 
+    /// <summary>
+    /// 根据电影名称获得指定电影信息,模糊查询
+    /// </summary>
+    /// <param name="name">电影名称</param>
+    /// <returns>
+    /// 返回电影json
+    /// </returns>
+    [HttpGet("getMovieByName/{name}")]
+    public async Task<IActionResult> GetMovieByName(string name)
+    {
+        var movies = await _db.Movies.Where(c => c.Name.Contains(name)).ToListAsync();
+        if (movies == null || movies!.Count == 0)
+        {
+            return new JsonResult(new GetMovieByNameResponse
+            {
+                Status = "4001",
+                Message = "电影不存在"
+            });
+        }
+
+        var response = new GetMovieByNameResponse
+        {
+            Status = "10000",
+            Message = "查询成功",
+            Movies = movies!
+        };
+
+        return new JsonResult(response);
+
+    }
+
+    /// <summary>
+    /// 通过特点搜索到电影院列表
+    /// </summary>
+    /// <param name="tags"></param>
+    /// <returns></returns>
+    [HttpGet("getMovieByTags/{tags}")]
+    public async Task<IActionResult> GetMovieByTags(string tags)
+    {
+        var movies = await _db.Movies.Where(c => c.Tags != null && c.Tags.Contains(tags)).ToListAsync();
+
+        if (movies.Count == 0)
+        {
+            return new JsonResult(new GetMovieByTagsResponse
+            {
+                Status = "4001",
+                Message = "电影不存在"
+            });
+        }
+
+        var response = new GetMovieByTagsResponse
+        {
+            Status = "10000",
+            Message = "查询成功",
+            Movies = movies.Select(c => c != null ? (Movie?)c : null).ToList()
+        };
+
+        return new JsonResult(response);
+    }
+
+    /// <summary>
+    /// 通过ID删除对应电影
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    [HttpDelete("deleteMovieById/{id}")]
+    public async Task<IActionResult> DeleteMovieById(string id)
+    {
+        var movie = await _db.Movies.FindAsync(id);
+
+        if (movie == null)
+        {
+            return new JsonResult(new DeleteMovieByIdResponse
+            {
+                Status = "4001",
+                Message = "该电影不存在"
+            });
+        }
+
+        _db.Movies.Remove(movie);
+        await _db.SaveChangesAsync();
+
+        var response = new DeleteMovieByIdResponse
+        {
+            Status = "10000",
+            Message = "删除成功"
+        };
+
+        return new JsonResult(response);
+
+    }
+
+    /// <summary>
+    /// 添加电影
+    /// </summary>
+    /// <param name="request"></param>
+    /// <returns>响应信息</returns>
+    [HttpPut("add")]
+    public async Task<IActionResult> AddMovie(AddMovieRequest request)
+    {
+        try
+        {
+            var movie = new Movie
+            {
+                MovieId = request.MovieId,
+                Name = request.Name,
+                Duration = request.Duration,
+                Instruction = request.Instruction,
+                PostUrl = request.PostUrl,
+                Tags = request.Tags,
+                ReleaseDate = DateTime.ParseExact(request.ReleaseDate, "yyyy-MM-dd", System.Globalization.CultureInfo.CurrentCulture),
+                RemovalDate = DateTime.ParseExact(request.RemovalDate, "yyyy-MM-dd", System.Globalization.CultureInfo.CurrentCulture)
+            };
+            await _db.AddAsync(movie);
+            await _db.SaveChangesAsync();
+            return new JsonResult(new AddMovieResponse
+            {
+                Status = "10000",
+                Message = "添加成功",
+                Movie = movie
+            });
+        }
+        catch (Exception ex)
+        {
+            return new JsonResult(new AddMovieResponse
+            {
+                Status = "10001",
+                Message = "添加失败：" + ex.Message
+            });
+        }
+    }
 }
+
--- Cinema/DTO/MovieService/MovieResponse.cs
+++ Cinema/DTO/MovieService/MovieResponse.cs
@@ -4,14 +4,14 @@ using Cinema.Entities;
 namespace Cinema.DTO.MovieService;
 
 /// <summary>
-/// 通过电影id查询演员列表
+/// 添加电影响应体
 /// </summary>
-public class GetActorsByMovieIdResponse : IAPIResponse
+public class AddMovieResponse : IAPIResponse
 {
-    /// <sumamary>
-    /// 演员实体
-    /// </sumamary>
-    [JsonPropertyName("act")] public ICollection<Act>? Act { get; set; }
+    /// <summary>
+    /// 电影实体
+    /// </summary>
+    [JsonPropertyName("movie")] public Movie? Movie { get; set; }
 
     /// <summary>
     /// 响应状态
@@ -19,7 +19,91 @@ public class GetActorsByMovieIdResponse : IAPIResponse
     public string Status { get; set; } = String.Empty;
 
     /// <summary>
-    /// 响应信息
+    /// 响应消息
+    /// </summary>
+    public string Message { get; set; } = String.Empty;
+}
+
+/// <summary>
+/// 通过id查询电影响应
+/// </summary>
+public class GetMovieByIdResponse : IAPIResponse
+{
+    /// <summary>
+    /// 电影院实体
+    /// </summary>
+    [JsonPropertyName("movie")] public Movie? Movie { get; set; }
+
+    /// <summary>
+    /// 响应状态
+    /// </summary>
+    public string Status { get; set; } = String.Empty;
+
+    /// <summary>
+    /// 响应消息
+    /// </summary>
+    public string Message { get; set; } = String.Empty;
+}
+
+/// <summary>
+/// 通过电影名查询电影响应，模糊查找
+/// </summary>
+public class GetMovieByNameResponse : IAPIResponse
+{
+    /// <summary>
+    /// 查询得到的电影院实体列表
+    /// </summary>
+    [JsonPropertyName("movie")] public List<Movie?>? Movies { get; set; }
+
+    /// <summary>
+    /// 响应状态
+    /// </summary>
+    public string Status { get; set; } = String.Empty;
+
+    /// <summary>
+    /// 响应消息
+    /// </summary>
+    public string Message { get; set; } = String.Empty;
+}
+
+/// <summary>
+/// 通过特点查询电影响应，模糊查找
+/// </summary>
+public class GetMovieByTagsResponse : IAPIResponse
+{
+    /// <summary>
+    /// 查询得到的电影院实体列表
+    /// </summary>
+    [JsonPropertyName("movies")] public List<Movie?>? Movies { get; set; }
+
+    /// <summary>
+    /// 响应状态
+    /// </summary>
+    public string Status { get; set; } = String.Empty;
+
+    /// <summary>
+    /// 响应消息
+    /// </summary>
+    public string Message { get; set; } = String.Empty;
+}
+
+/// <summary>
+/// 通过id删除对应电影
+/// </summary>
+public class DeleteMovieByIdResponse : IAPIResponse
+{
+    /// <summary>
+    /// 电影院实体
+    /// </summary>
+    [JsonPropertyName("movie")] public Movie? Movie { get; set; }
+
+    /// <summary>
+    /// 响应状态
+    /// </summary>
+    public string Status { get; set; } = String.Empty;
+
+    /// <summary>
+    /// 响应消息
     /// </summary>
     public string Message { get; set; } = String.Empty;
 }
