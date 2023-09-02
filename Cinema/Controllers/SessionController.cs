0a09753e5105eda48bd11037dde705215d13c282
Zhang Yao <cineazhan@gmail.com>
2023-09-03 00:21:58 +0800

    初步完成购票系统

--- Cinema/Controllers/SessionController.cs
+++ Cinema/Controllers/SessionController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Cinema.DTO;
+using Cinema.DTO.SessionService;
 using Cinema.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -50,6 +51,33 @@ namespace Namespace
                 select g;
 
             return APIDataResponse<Dictionary<DateTime, List<Session>>>.Success(groupedSessions.ToDictionary(g=>g.Key,g=>g.ToList()));
+        }
+
+        /// <summary>
+        /// 获取排片详情，客户端接口
+        /// </summary>
+        /// <param name="movieId"></param>
+        /// <param name="cinemaId"></param>
+        /// <param name="hallId"></param>
+        /// <param name="rawStartTime"></param>
+        /// <returns></returns>
+        [HttpGet("detail")]
+        [ProducesDefaultResponseType(typeof(APIDataResponse<Session>))]
+        public async Task<IAPIResponse> GetSessionDetail([FromQuery] string movieId, [FromQuery] string cinemaId, [FromQuery] string hallId, [FromQuery] long rawStartTime)
+        {
+            var startTime = DateTime.UnixEpoch.AddSeconds(rawStartTime/1000).ToLocalTime();
+            var session = await _db.Sessions
+                .Include(s=>s.MovieBelongsTo)
+                .Include(s => s.HallLocatedAt)
+                .ThenInclude(s => s.CinemaBelongTo)
+                .FirstOrDefaultAsync(s => s.MovieId == movieId && s.CinemaId == cinemaId && s.HallId == hallId && s.StartTime == startTime);
+            if (session == null) 
+                return APIResponse.Failaure("4001","找不到对应排片");
+            return APIDataResponse<SessionDetailResponse>.Success(new SessionDetailResponse
+            {
+                Session = session,
+                Movie = session.MovieBelongsTo
+            });
         }
     }
 }
---
+++ Cinema/DTO/SessionService/SessionDetailResponse.cs
@@ -0,0 +1,20 @@
+﻿using Cinema.Entities;
+
+namespace Cinema.DTO.SessionService
+{
+    /// <summary>
+    /// 排片信息结构体
+    /// </summary>
+    public class SessionDetailResponse
+    {
+        /// <summary>
+        /// 排片
+        /// </summary>
+        public Session Session { get; set; } = null!;
+
+        /// <summary>
+        /// 所属影片
+        /// </summary>
+        public Movie Movie { get; set; } = null!;
+    }
+}
