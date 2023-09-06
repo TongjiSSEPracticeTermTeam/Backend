ae7a18bd80f630e52bd5c1543dd0d1a2dcd1695d
BayInl <dry330500@gmail.com>
2023-09-06 15:15:28 +0800

    添加了SessionDTO

--- Cinema/Controllers/SessionController.cs
+++ Cinema/Controllers/SessionController.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Cinema.DTO;
+using Cinema.DTO.SessionService;
 using Cinema.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using TencentCloud.Mna.V20210119.Models;
 
 namespace Namespace
 {
@@ -50,6 +52,36 @@ namespace Namespace
                 select g;
 
             return APIDataResponse<Dictionary<DateTime, List<Session>>>.Success(groupedSessions.ToDictionary(g=>g.Key,g=>g.ToList()));
+        }
+
+        /// <summary>
+        /// 尝试在某个影厅的某个时间点排片
+        /// </summary>
+        /// <returns></returns>
+        [HttpPut]
+        [ProducesDefaultResponseType(typeof(APIResponse))]
+        public async Task<IAPIResponse> PutSession([FromBody] SessionDTO data)
+        {
+            // 先判断影厅是否存在
+            var hall = await _db.Halls.FirstOrDefaultAsync(h => h.Id == data.HallId && h.CinemaId == data.CinemaId);
+            if (hall == null)
+                return APIResponse.Failaure("40001","影厅不存在");
+            // 之后判断电影是否存在
+            var movie = await _db.Movies.FirstOrDefaultAsync(m => m.MovieId == data.MovieId);
+            if (movie == null)
+                return APIResponse.Failaure("40002", "电影不存在");
+            // 再判断拍片时间是否在电影上下映之间
+            if (data.StartTime < movie.ReleaseDate || data.StartTime > movie.RemovalDate)
+                return APIResponse.Failaure("40003", "排片时间不在电影上映时间内");
+            //最后判断拍片时间是否已经被占用
+            var existed = await _db.Sessions
+                .AnyAsync(s => s.HallId == data.HallId
+               && s.CinemaId == data.CinemaId
+               && s.StartTime == data.StartTime);
+            if (existed)
+                return APIResponse.Failaure("40004", "该时间段已被排片");
+            // 如果都没有问题，就创建排片
+
         }
     }
 }
--- Cinema/DTO/SessionService/SessionDTO.cs
+++ Cinema/DTO/SessionService/SessionDTO.cs
@@ -1,7 +1,65 @@
-﻿namespace Cinema.DTO.SessionService
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Cinema.Entities;
+
+namespace Cinema.DTO.SessionService
 {
+    /// <summary>
+    /// 排片表
+    /// </summary>
     public class SessionDTO
     {
 
+        /// <summary>
+        /// 电影ID
+        /// </summary>
+        [Required][Column("MOVIE_ID")] public string MovieId { get; set; } = String.Empty;
+
+        /// <summary>
+        /// 影院ID
+        /// </summary>
+        [Required][Column("CINEMA_ID")] public string CinemaId { get; set; } = String.Empty;
+
+        /// <summary>
+        /// 影厅ID
+        /// </summary>
+        [Required][Column("HALL_ID")] public string HallId { get; set; } = String.Empty;
+
+        /// <summary>
+        /// 起始时间
+        /// </summary>
+        [Required]
+        [Column("START_TIME")]public DateTime StartTime { get; set; } = new DateTime();
+
+        /// <summary>
+        /// 上座率
+        /// </summary>
+        [Column("ATTENDANCE")]public float? Attendence { get; set; }
+
+        /// <summary>
+        /// 票价
+        /// </summary>
+        [Required][Column("PRICE")]public float Price { get; set; }
+
+        /// <summary>
+        /// 默认构造
+        /// </summary>
+        public SessionDTO()
+        {
+        }
+
+        /// <summary>
+        /// 由Session实体构造
+        /// </summary>
+        /// <param name="entity"></param>
+        public SessionDTO(Session entity)
+        {
+            MovieId=entity.MovieId;
+            CinemaId=entity.CinemaId;
+            HallId=entity.HallId;
+            StartTime=entity.StartTime;
+            Attendence=entity.Attendence;
+            Price=entity.Price;
+        }
     }
 }
